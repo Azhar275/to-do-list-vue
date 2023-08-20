3f6cc884bb7a36dab8a80f02cac49ac5d016e35d
Azhar275 <azhartawakkalneeds@gmail.com>
2023-08-20 15:06:33 +0700

    Fix home

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const username = computed(() => d$auth().g$user?.id)
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Kamu berhasil Dora!" />
+      <HelloWorld msg="You Succeed Access my To-Do List!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
